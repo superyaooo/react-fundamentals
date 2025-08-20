ba226ac6bb28e57ca941eb76b17a0d788dcb9724
superyaooo <superyaooo@gmail.com>
2025-08-20 18:36:58 -0500

    props & children prop

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import './App.css'
 function App() {
   return (
     <>
-      <Banner headerText="looking for a mushroom to rent" />
+      {/* <Banner headerText="looking for a mushroom to rent" /> */}
+      <Banner>
+        <div>ISO a mushroom rental.</div>
+      </Banner>
     </>
   )
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,14 @@
 import logo from '../assets/Logo.jpg'
 import {logo as logoClass} from './Banner.module.css'
 
-const Banner = ({headerText}) => {
+const Banner = ({children}) => {
   return (
     <header className='row mb-4'>
       <div className='col-5'>
         <img src={logo} alt="logo" className={logoClass}/>
       </div>
       <div className='col-7 mt-5'>
-        {headerText}
+        {children}
       </div>
     </header>
   );
