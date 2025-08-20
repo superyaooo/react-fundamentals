894e8b848ce14f70c385a4baa1d81ca6bd679ea4
superyaooo <superyaooo@gmail.com>
2025-08-20 18:27:26 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Banner from "./components/Banner"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Banner headerText="looking for a mushroom to rent" />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Banner.jsx
@@ -0,0 +1,17 @@
+import logo from '../assets/Logo.jpg'
+import {logo as logoClass} from './Banner.module.css'
+
+const Banner = ({headerText}) => {
+  return (
+    <header className='row mb-4'>
+      <div className='col-5'>
+        <img src={logo} alt="logo" className={logoClass}/>
+      </div>
+      <div className='col-7 mt-5'>
+        {headerText}
+      </div>
+    </header>
+  );
+}
+
+export default Banner;
